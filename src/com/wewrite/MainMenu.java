23c569f897e3ffb2feae1b346e730771d7a8d5d8
calebyoon/n <csyoon@umich.edu/n>
2013-09-25 17:47:38 -0400

    some of the collabrify stuff. not tested yet but most recent stuff

--- src/com/wewrite/MainMenu.java
+++ src/com/wewrite/MainMenu.java
@@ -1,30 +1,59 @@
 package com.wewrite;
 
-import android.os.Bundle;
+
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.view.KeyEvent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import edu.umich.imlc.collabrify.client.CollabrifyClient;
 import edu.umich.imlc.collabrify.client.CollabrifyListener;
-import edu.umich.imlc.collabrify.client.CollabrifyParticipant;
-import edu.umich.imlc.collabrify.client.CollabrifySession;
 import edu.umich.imlc.collabrify.client.exceptions.CollabrifyException;
 
 public class MainMenu extends Activity
 {
+  private static String TAG = "WeWrite";
+  
   private CollabrifyClient myClient;
   private CollabrifyListener collabrifyListener;
   boolean getLatestEvent = false;
-  
+  private String sessionName;
+  private Button createSession;
+  private ArrayList<String> tags = new ArrayList<String>();
   
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.welcome_screen_layout);
+    createSession = (Button) findViewById(R.id.createSession);
+    createSession.setOnClickListener(new OnClickListener()
+    {
+      @Override
+      public void onClick(View v)
+      {
+        try
+        {
+          Random rand = new Random();
+          sessionName = "csyoon.amchr " + rand.nextInt(Integer.MAX_VALUE);         
+          myClient.createSession(sessionName, tags, null, 0);
+          Log.i(TAG, "session name is " + sessionName);
+        }
+        catch( CollabrifyException e )
+        {
+          Log.e(TAG, "error", e);
+        }
+      }
+    });
+    
     try
     {
       myClient = new CollabrifyClient(this, "user email", "user display name",
@@ -36,6 +65,8 @@ public class MainMenu extends Activity
       e.printStackTrace();
     }
   }
+  
+ 
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu)
