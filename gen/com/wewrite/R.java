0c8cc01c45415bf2a1c55e059858cde33c237c9f
calebyoon/n <csyoon@umich.edu/n>
2013-09-27 16:55:28 -0400

    not using welcome screen anymore. main activity for everything

--- gen/com/wewrite/R.java
+++ gen/com/wewrite/R.java
@@ -25,9 +25,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
         public static final int createSession=0x7f080001;
         public static final int editTextSimple=0x7f080000;
+        public static final int getSessionButton=0x7f080002;
+        public static final int joinSession=0x7f080003;
+        public static final int leaveSession=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -39,7 +41,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f040005;
         public static final int app_name=0x7f040000;
+        public static final int createSession=0x7f040006;
         public static final int hello_world=0x7f040001;
+        public static final int joinSession=0x7f040007;
+        public static final int leaveSession=0x7f040008;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
         public static final int url=0x7f040004;
